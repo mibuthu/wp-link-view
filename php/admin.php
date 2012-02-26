f38a1b93d80d7d49a2aa3f963c78d45393dada52
Michael Burtscher <mike@mbnet.at>
2012-02-26 12:10:33 +0100

    added shortcode attribute "show_img", updated description in admin page, set up an array for all attribute-datas

--- php/admin.php
+++ php/admin.php
@@ -8,46 +8,48 @@ class admin {
 		if (!current_user_can('edit_posts'))  {
 			wp_die( __('You do not have sufficient permissions to access this page.') );
 		}
+		require_once('sc_linkview.php');
 
 		$out ='
 			<div class="wrap nosubsub" style="padding-bottom:15px">
 			<div id="icon-link-manager" class="icon32"><br /></div><h2>Link View</h2>
 			</div>
 			<h4>Usage of the <code>[linkview]</code> shortcode</h4>
-			<div style="padding:0 20px">
-				<h4>A short introduction</h4>
-				<p>Link View works using a "shortcode". Shortcodes are snippets of pseudo code that are placed in blog posts, pages and on some forums to easily render HTML output.</p>
-				<p>The following is the basic form of a shortcode:</p>
-				<p><code>[shortcodename]</code></p>
+			<div style="padding:0 15px">
+				<p>Link View works using a "shortcode". Shortcodes are snippets of pseudo code that are placed in blog posts or pages to easily render HTML output.</p>
+				<p>The following is the basic form of the Link View shortcode:</p>
+				<p><code>[linkview]</code></p>
 				<p></p>
-				<p>To facilitate customization of shortcodes, parameters are used. Shortcode parameters are entered in the following format:<p>
-				<p><code>[shortcodename parametername=parametervalue]</code></p>
-				<p></p>
-				<p>The <code>[linkview]</code> shortcode including an example parameter called "cat_name" looks like this:</p>
+				<p>To facilitate customization of shortcodes, attributes are used. The <code>[linkview]</code> shortcode including the attribute called "cat_name" looks like this:</p>
 				<p><code>[linkview cat_name=Sponsors]</code></p>
-				<p>Below is a list of all the supported parameters and their functions:</p>
+				<p>Below is a list of all the supported attributes with their descriptions and available options:</p>
 				<style type="text/css">
 					<!--
 					#lvadmintable {border:1px solid #aaa;border-collapse:collapse}
-					#lvadmintable th {border:1px solid #aaa;background:#eeeeee;}
-					#lvadmintable td {border:1px solid #aaa;padding:4px 3px;vertical-align: top}
-					#lvadmintable td.secondrow {padding:4px 4px;}
+					#lvadmintable th {border:1px solid #aaa;padding:3px 4px;background:#eeeeee;}
+					#lvadmintable td {border:1px solid #aaa;padding:4px 5px;vertical-align: top}
 					-->
 				</style>
 				<table id="lvadmintable">
 					<tr>
-						<th>Parameter name</th>
-						<th>Parameter options</th>
+						<th>Attribute name</th>
+						<th>Value options</th>
+						<th>Default value</th> 
 						<th>Description</th>
-					</tr>
+					</tr>';
+
+		foreach( sc_linkview::$attr as $aname => $a ) {
+			$out .= '
 					<tr>
-						<td>cat_name</td>
-						<td class="secondrow">Name(s)</td>
-						<td>Use this parameter to specify what categories should be shown by name. For example <code>[linkview catname=Sponsors]</code>. If the catname has spaces, simply wrap the name in quotes.<br />
-							Example: <code>[linkview catname="Social Media"]</code></td> 
-					</tr>
+						<td>'.$aname.'</td>
+						<td>'.$a['val'].'</td>
+						<td>'.$a['std_val'].'</td>
+						<td>'.$a['desc'].'</td>
+					</tr>';
+		}
+		$out .= '
 				</table>
-				<p>In this early version there only this parameter available. Other parameters will be added in a later releases.</p>
+				<p>In this early version there are only these 2 attributes available. More attributes will be added in a later release.</p>
 			</div>';
 		echo $out;
 	}
--- php/sc_linkview.php
+++ php/sc_linkview.php
@@ -3,19 +3,34 @@
 // This class handles the shortcode [linkview]
 class sc_linkview {
 
+	public static $attr = array(
+		'cat_name' 	=> array(	'val'		=> 'Name',
+								'std_val'	=> '',
+								'desc'		=> 'This attribute specifies what categories should be shown by name. If you leave the attribute empty all categories are shown.<br />
+												For example <code>[linkview catname=Sponsors]</code>. If the catname has spaces, simply wrap the name in quotes.<br />
+												Example: <code>[linkview catname="Social Media"]</code>' ),
+		'show_img'	=> array(	'val'		=> '0 ... false<br />1 ... true',
+								'std_val'	=> '0',
+								'desc'		=> 'This attribute specifies if the image is displayed instead of the name. This attribute is only considered for links where an image was set.' )
+		);
+
 	// main function to show the rendered HTML output
 	public static function show_html( $atts ) {
 		
-		$a = shortcode_atts( array(
-			'cat_name' => '',
-			'test' => 'test'
-		), $atts );
+		// check attributes
+		$std_values = array();
+		foreach( sc_linkview::$attr as $aname => $attribute ) {
+			$std_values[$aname] = $attribute['std_val'];
+		}
+		$a = shortcode_atts( $std_values, $atts );
 
+		// generate output
 		$out = "";
-		$out .= 'cat_name: {'.$a['cat_name'].'}<br />';
-		$out .= 'test: {'.$a['test'].'}<br />';
+		//foreach( sc_linkview::$attr as $aname => $attribute ) {
+		//	$out .= $aname.' {'.$a[$aname].'}<br />';
+		//}
 		ob_start();
-		wp_list_bookmarks( 'category_name='.$a['cat_name'] );
+		wp_list_bookmarks( 'category_name='.$a['cat_name'].'&show_images='.$a['show_img'] );
 		$out .= ob_get_contents();
 		ob_end_clean();
 /*		$links = get_bookmarks('');
