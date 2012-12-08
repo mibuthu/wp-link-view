51cb93a36a05c504c92b4db7d6c782d0f38cfc8d
Michael Burtscher <mike@mbnet.at>
2012-12-08 21:33:16 +0100

    moved the rest of the admin styles to the css file; changed lvadmin table from id to class

--- php/admin.php
+++ php/admin.php
@@ -24,11 +24,11 @@ class lv_admin {
 		}
 
 		$out ='
-			<div class="wrap nosubsub" style="padding-bottom:15px">
+			<div class="wrap nosubsub">
 			<div id="icon-link-manager" class="icon32"><br /></div><h2>Link View</h2>
 			</div>
 			<h4>Usage of the <code>[linkview]</code> shortcode</h4>
-			<div style="padding:0px 0px 10px 15px">
+			<div class="lvcontent">
 				<p>Link View works using a "shortcode". Shortcodes are snippets of pseudo code that are placed in blog posts or pages to easily render HTML output.</p>
 				<p>The following is the basic form of the Link View shortcode:</p>
 				<p><code>[linkview]</code></p>
@@ -37,7 +37,7 @@ class lv_admin {
 				The <code>[linkview]</code> shortcode including the attributes "cat_name" and "show_img" looks like this:</p>
 				<p><code>[linkview cat_name=Sponsors show_img=1]</code></p>
 				<p>Below is a list of all the supported attributes with their descriptions and available options:</p>
-				<table id="lvadmintable">
+				<table class="lvadmintable">
 					<tr>
 						<th>Attribute name</th>
 						<th>Value options</th>
@@ -58,7 +58,7 @@ class lv_admin {
 				</table>
 			</div>
 			<h4>LinkView Widget</h4>
-			<div style="padding:0px 0px 10px 15px">
+			<div class="lvcontent">
 				There is also a Widget available which allows you to use the shortcode in sidebars.<br />
 				Goto Appearance -> Widgets and add the "LinkView"-Widget in one of your Sidebars.<br />
 				The Widget allows you to enter a title. Additionally you can add all the required shortcode attributes in the "Shortcode attributes" field.<br />
