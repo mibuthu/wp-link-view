70ca1be395de84fb07df19add7ae971baf0e3a94
mibuthu <mike@mbnet.at>
2018-09-17 15:17:26 +0200

    refactor admin/admin.php
    * switch to WordPress coding standard
    * fixed/added doc blocks
    * changed some function names
    * use php strict_types mode
    * update code comments

--- admin/admin.php
+++ admin/admin.php
@@ -1,58 +1,136 @@
 <?php
-if(!defined('WPINC')) {
-	die;
+/**
+ * LinkViews Main Admin Class
+ *
+ * @package link-view
+ */
+
+declare(strict_types=1);
+if ( ! defined( 'WP_ADMIN' ) ) {
+	exit;
 }
 
-require_once(LV_PATH.'includes/options.php');
+require_once LV_PATH . 'includes/options.php';
 
-// This class handles all available admin pages
+/**
+ * LinkViews Main Admin Class
+ *
+ * This class handles all LinkView admin pages.
+ */
 class LV_Admin {
+
+	/**
+	 * Class singleton instance reference
+	 *
+	 * @var self
+	 */
 	private static $instance;
+
+	/**
+	 * Options class instance reference
+	 *
+	 * @var LV_Options
+	 */
 	private $options;
 
-	private function __construct() {
-		$this-> options = LV_Options::get_instance();
-	}
 
+	/**
+	 * Singleton provider and setup
+	 *
+	 * @return self
+	 */
 	public static function &get_instance() {
-		// singleton setup
-		if(!isset(self::$instance)) {
-			self::$instance = new self;
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
 
-	public function init_admin_page() {
-		// Register actions
-		add_action('admin_menu', array(&$this, 'register_pages'));
-	}
 
 	/**
-	 * Add and register all admin pages in the admin menu
+	 * Class constructor which initializes required variables
 	 */
-	public function register_pages() {
-		$page = add_submenu_page('link-manager.php', sprintf(__('About %1$s','link-view'), 'LinkView'), sprintf(__('About %1$s','link-view'), 'LinkView'), $this->options->get('lv_req_cap'), 'lv_admin_about', array(&$this, 'show_about_page'));
-		add_action('admin_print_scripts-'.$page, array(&$this, 'embed_about_scripts'));
-		$page = add_submenu_page('options-general.php', sprintf(__('%1$s Settings','link-view'), 'LinkView'), 'LinkView', 'manage_options', 'lv_admin_options', array(&$this, 'show_settings_page'));
-		add_action('admin_print_scripts-'.$page, array(&$this, 'embed_settings_scripts'));
+	private function __construct() {
+		$this->options = LV_Options::get_instance();
 	}
 
+
+	/**
+	 * Initialize the admin page (register required admin actions)
+	 *
+	 * @return void
+	 */
+	public function init_admin_page() {
+		add_action( 'admin_menu', array( &$this, 'register_pages' ) );
+	}
+
+
+	/**
+	 * Add and register all pages in the admin menu
+	 */
+	public function register_pages() {
+		$page = add_submenu_page(
+			'link-manager.php',
+			sprintf( __( 'About %1$s', 'link-view' ), 'LinkView' ),
+			sprintf( __( 'About %1$s', 'link-view' ), 'LinkView' ),
+			$this->options->get( 'lv_req_cap' ),
+			'lv_admin_about',
+			array( &$this, 'show_about_page' )
+		);
+		add_action( 'admin_print_scripts-' . $page, array( &$this, 'embed_about_styles' ) );
+		$page = add_submenu_page(
+			'options-general.php',
+			sprintf( __( '%1$s Settings', 'link-view' ), 'LinkView' ),
+			'LinkView', 'manage_options',
+			'lv_admin_options',
+			array( &$this, 'show_settings_page' )
+		);
+		add_action( 'admin_print_scripts-' . $page, array( &$this, 'embed_settings_styles' ) );
+	}
+
+
+	/**
+	 * Show the plugins about page
+	 *
+	 * @return void
+	 */
 	public function show_about_page() {
-		require_once(LV_PATH.'admin/includes/admin-about.php');
+		require_once LV_PATH . 'admin/includes/admin-about.php';
 		LV_Admin_About::get_instance()->show_page();
 	}
 
+
+	/**
+	 * Show the plugins settings page
+	 *
+	 * @return void
+	 */
 	public function show_settings_page() {
-		require_once(LV_PATH.'admin/includes/admin-settings.php');
+		require_once LV_PATH . 'admin/includes/admin-settings.php';
 		LV_Admin_Settings::get_instance()->show_page();
 	}
 
-	public function embed_about_scripts() {
-		wp_enqueue_style('linkview_admin_about', LV_URL.'admin/css/admin_about.css');
+
+	/**
+	 * Embed the plugins about page styles
+	 * TODO: move to admin about class
+	 *
+	 * @return void
+	 */
+	public function embed_about_styles() {
+		wp_enqueue_style( 'lv_admin_about', LV_URL . 'admin/css/admin_about.css' );
 	}
 
-	public function embed_settings_scripts() {
-		wp_enqueue_style('linkview_admin_settings', LV_URL.'admin/css/admin_settings.css');
+
+	/**
+	 * Embed the plugins settings page styles
+	 * TODO: move to admin settings class
+	 *
+	 * @return void
+	 */
+	public function embed_settings_styles() {
+		wp_enqueue_style( 'lv_admin_settings', LV_URL . 'admin/css/admin_settings.css' );
 	}
-} // end class LV_Admin
-?>
+
+}
+
