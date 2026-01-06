6dcd71c33787954cc6bc76e5693eccede0bcd90c
Michael Burtscher <mike@bbnet.at>
2026-01-06 08:42:25 +0100

    Added rector to composer.json and the default rector config file
    No modifications are done yet.

---
+++ rector.php
@@ -0,0 +1,15 @@
+<?php // phpcs:disable
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+// use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withPhpSets()
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0)
+;
