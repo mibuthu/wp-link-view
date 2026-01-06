0e95366d06c4a47afca480d6d46485462c9fa436
Michael Burtscher <mike@bbnet.at>
2026-01-06 13:22:28 +0100

    Some modification in rector config file and added wordpress rector rules in composer.json and rector config

--- rector.php
+++ rector.php
@@ -3,15 +3,29 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Fsylum\RectorWordPress\Set\WordPressSetList;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
     ])
     ->withPhpSets()
-    ->withTypeCoverageLevel(50)
-    ->withDeadCodeLevel(50)
-    ->withCodeQualityLevel(50)
+    ->withAttributesSets()
+    ->withPreparedSets(
+        codeQuality: false,
+        codingStyle: false,
+        deadCode: true,
+        instanceOf: false,
+        earlyReturn: false,
+        naming: false,
+        privatization: false,
+        rectorPreset: false,
+        strictBooleans: false,
+        typeDeclarations: true,
+    )
+    ->withSets([
+        WordPressSetList::WP_6_8
+    ])
     ->withSkip([
         ClassPropertyAssignToConstructorPromotionRector::class,
     ])
